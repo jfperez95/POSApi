d859249fee3b778d6c5cde8f736c90061dafc877
jfperez <jfperez9510@gmail.com>
2026-01-07 21:12:44 -0500

    Correccion

--- POS.Application/Dtos/Response/CategoryResponseDto.cs
+++ POS.Application/Dtos/Response/CategoryResponseDto.cs
@@ -5,7 +5,7 @@
         public int CategoryId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public int AuditCreateUser { get; set; }
+        public DateTime AuditCreateDate { get; set; }
         public int State { get; set; }
         public string? StateCategory { get; set; }
     }
